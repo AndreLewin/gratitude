d6648e5c56a2114aec91a96730388a8b07b73368
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2022-02-18 12:57:01 +0100

    prepare project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  )
 }
 
 export default MyApp
