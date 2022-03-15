7b30084deef7947069b954e7dde63a457ea4c618
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2022-03-15 18:03:15 +0100

    answers to frequently asked questions

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,11 @@ send them a message. It's nice to know w
 
 -- What is gratitude journaling
 https://en.wikipedia.org/wiki/Gratitude_journal
-important quotes:
 
-https://www.youtube.com/watch?v=CUXAfSv157o
-
+https://www.youtube.com/watch?v=WPPPFqsECz0
 https://www.youtube.com/watch?v=GZghu_xFRM8
+
+
 
 
 -- Why this website (social)
@@ -43,11 +43,45 @@ GJ is (and will stay) a tool focused on personal gratitude journaling and happin
 -- 
 
 -- Friends and Public system
+you can send messages to only your friends
 
 -- Permission system
 
 -- There is also a public channel, where you can see what
 -- Understand yourself better. Know what is important for your friends and what makes them happy.
+
+*/
+
+
+/*
+- How often should I write a gratitude message?
+
+Some people recommend writing gratitude messages daily. Some other people recommend thrice a week. Personally, I try to write at least one message every day ON AVERAGE. My daily goal helps me to stay attentive to positive emotions, and it makes it easier to turn gratitude journaling into a routine. It's a loose and flexible goal: it is okay if I write nothing for several days, then a lot in one day.
+
+But that's my personal goal. In general, I recommend to NOT have a schedule. (Or at least not a schedule that has been imposed on you by someone else.) Everyone is different. The frequency of your gratitude events might not match a preset plan. And if you can't keep with your schedule, you will get frustrated. Allow your usage of the gratitude journal to adapt to yourself.
+
+I recommend writing down your gratitude shortly after you are experiencing it. That way, you will not forget to write it down, and because it's still fresh in your memory, you will be able to describe and explain it with more details. Don't force yourself to express gratitude. Let it come to you naturally. If you are feeling happy, it's maybe the moment to write a new entry.
+
+
+- What should I write over?
+
+Everything that you are grateful for. What is being grateful? That's when you have a pleasant feeling when you think about something you have, received or perceived.
+
+Please don't use the website to boast about all your achievements and everything you were able to buy. Gratefulness is not something you can buy or force to happen. Also, don't confuse happiness and gratefulness. Gratefulness is more about being content of all the small things that are happening around you in an unpredicted way.
+
+Try to get into the details. What precisely made you grateful and why? The more you explore, the easier it will be to reproduce the positive emotions.
+
+
+
+- How often should I read my previous gratitude message?
+
+At least one time. Reviewing (re-reading) your gratitude messages is highly recommended. By seeing the whole picture on the scale of a week or a month, you will remember (or realize) what makes you happy, who makes you happy and what actually matters to you.
+
+Personally, I read the gratitude messages of the last week on every Sunday morning. (Sometimes I also randomly jump weeks and months in the past). Then I start a chat with the people that made me happy. This routine made me more pro-social. Also, people really enjoy hearing that someone is grateful for them. Don't keep all the pleasure for yourself. :)
+
+Tip: re-read the gratitude messages of your friends, and talk also about these! It's a very good way to know your friends.
+
+It is also a good idea to read your gratitude messages when you are feeling down or depressed. Reading about your previous gratitude events will make you experience them partially a second time. It's literally happiness on demand.
 
 */
 
