2bb6ccd402b93c3b6d11d2b5839617b3de6d306e
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2022-03-24 18:00:22 +0100

    call to action in the end of Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@mantine/core";
 import { useViewportSize } from "@mantine/hooks";
 import YoutubeEmbed from "components/YoutubeEmbed";
+import Link from "next/link";
 
 export default function Index() {
   const { height, width } = useViewportSize()
@@ -17,7 +19,9 @@ export default function Index() {
           </div>
         </div>
         <div className="right" style={{ width: width > 1000 ? "50%" : "100%", marginTop: width > 1000 ? 0 : "40px" }}>
-          <YoutubeEmbed embedId="WPPPFqsECz0" />
+          <div style={{ maxWidth: "600px", width: "100%" }}>
+            <YoutubeEmbed embedId="WPPPFqsECz0" />
+          </div>
         </div>
       </div>
 
@@ -35,13 +39,18 @@ export default function Index() {
         </div>
       </div>
 
-      <div className="block" style={{ background: "rgb(69,149,164)" }}>
-        <div className="left" style={{ width: width > 1000 ? "50%" : "100%", height: "100%" }}>
-          Ready to try? Button
-        </div>
-        <div className="right" style={{ width: width > 1000 ? "50%" : "100%", marginTop: width > 1000 ? 0 : "40px" }}>
-          Want to see example of gratitude message ?
-        </div>
+      <div className="last-block" style={{ background: "rgb(69,149,164)" }}>
+        <div className="title" style={{ margin: "0px 10px 5px 10px" }}>Ready to try?</div>
+        <Link href="/login" passHref>
+          <Button style={{ margin: "5px 10px 5px 10px" }} color="cyan" variant="light" component="a" leftIcon={<svg viewBox="0 0 256 256" height="20px" width="20px"><path fill="currentColor" d="m144.5 136.5l-42 42A12 12 0 0 1 94 182a12.2 12.2 0 0 1-8.5-3.5a12 12 0 0 1 0-17L107 140H24a12 12 0 0 1 0-24h83L85.5 94.5a12 12 0 0 1 17-17l42 42a12 12 0 0 1 0 17ZM192 28h-56a12 12 0 0 0 0 24h52v152h-52a12 12 0 0 0 0 24h56a20.1 20.1 0 0 0 20-20V48a20.1 20.1 0 0 0-20-20Z"></path></svg>}>
+            Start gratitude journaling
+          </Button>
+        </Link>
+        <Link href="/public" passHref>
+          <Button style={{ margin: "5px 10px 5px 10px" }} color="cyan" variant="light" component="a" leftIcon={<svg viewBox="0 0 256 256" height="20px" width="20px"><path fill="currentColor" d="m225.9 163.2l.3-1a103.7 103.7 0 0 0 0-68.4l-.3-1A104.4 104.4 0 0 0 128 24a104.2 104.2 0 0 0-97.9 68.8l-.3 1a103.7 103.7 0 0 0 0 68.4l.3 1A104.2 104.2 0 0 0 128 232a104.2 104.2 0 0 0 97.9-68.8ZM102.5 168h51a108.6 108.6 0 0 1-25.5 42.4a108.6 108.6 0 0 1-25.5-42.4Zm-4.2-16a126.4 126.4 0 0 1 0-48h59.4a127.6 127.6 0 0 1 2.3 24a126.8 126.8 0 0 1-2.3 24ZM40 128a90.3 90.3 0 0 1 3.3-24H82a145 145 0 0 0 0 48H43.3a90.3 90.3 0 0 1-3.3-24Zm113.5-40h-51A108.6 108.6 0 0 1 128 45.6A108.6 108.6 0 0 1 153.5 88Zm20.5 16h38.7a88.9 88.9 0 0 1 0 48H174a145 145 0 0 0 0-48Zm32.4-16h-36a128.7 128.7 0 0 0-24.1-46.1A88.6 88.6 0 0 1 206.4 88Zm-96.7-46.1A128.7 128.7 0 0 0 85.6 88h-36a88.6 88.6 0 0 1 60.1-46.1ZM49.6 168h36a128.7 128.7 0 0 0 24.1 46.1A88.3 88.3 0 0 1 49.6 168Zm96.7 46.1a128.7 128.7 0 0 0 24.1-46.1h36a88.3 88.3 0 0 1-60.1 46.1Z"></path></svg>}>
+            See some examples
+          </Button>
+        </Link>
       </div>
 
       <style jsx>
@@ -60,7 +69,7 @@ export default function Index() {
           }
 
           .title {
-            font-size: 40px;
+            font-size: 28px;
             line-height: 40px;
             font-family: Tahoma;
             text-shadow: 0px 1px 1px black;
@@ -78,6 +87,9 @@ export default function Index() {
           .right {
             padding-left: 60px;
             padding-right: 60px;
+
+            display: flex;
+            justify-content: center;
           }
 
           .center-vertically {
@@ -85,6 +97,20 @@ export default function Index() {
             display: flex;
             flex-direction: column;
             justify-content: center;
+          }
+
+          .last-block {
+            padding: 60px 60px 60px 60px;
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+          }
+
+          .icon {
+            width: 16px;
+            height: 16px;
+            margin-right: 8px;
+            margin-bottom: -2px;
           }
         `}
       </style>
