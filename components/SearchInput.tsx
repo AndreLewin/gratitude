2a8b4a0e391ea547cd8882ee055257ceb1a2943c
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2022-04-01 15:38:37 +0200

    debounced local search

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,31 +1,31 @@
 import { TextInput } from "@mantine/core";
-import { useCallback } from "react";
+import { useDebouncedValue } from "@mantine/hooks";
+import { useEffect, useState } from "react";
 import store from "store";
 import { supabase } from "utils/supabaseClient";
-
 
 export default function SearchInput() {
   const search = store(state => state.search)
   const set = store(state => state.set)
   const getGratitudes = store(state => state.getGratitudes)
 
-  // TODO: use debounced value
+  const [localSearch, setLocalSearch] = useState(search)
+  const [debouncedLocalSearch] = useDebouncedValue(localSearch, 300)
 
-  // onEffect on the debounced value: under
-
-  const changeSearchValue = useCallback<any>((search: string) => {
-    set({ search, gratitudes: [] })
+  useEffect(() => {
+    if (search === debouncedLocalSearch) return
+    set({ search: debouncedLocalSearch, gratitudes: [] })
     const user = supabase.auth.user()
     getGratitudes(user?.id ?? null)
-  }, [])
+  }, [debouncedLocalSearch])
 
   return (
     <div className="container">
       <TextInput
-        value={search}
+        value={localSearch}
         placeholder="Search in messages..."
         icon={<svg width="1em" height="1em" viewBox="0 0 256 256"><path fill="currentColor" d="m232.5 215.5l-40.7-40.7A94.9 94.9 0 0 0 212 116a96 96 0 1 0-96 96a94.9 94.9 0 0 0 58.8-20.2l40.7 40.7a12.1 12.1 0 0 0 17 0a12 12 0 0 0 0-17ZM44 116a72 72 0 1 1 72 72a72.1 72.1 0 0 1-72-72Z"></path></svg>}
-        onChange={(event) => changeSearchValue(event.currentTarget.value)}
+        onChange={(event) => setLocalSearch(event.currentTarget.value)}
       />
 
       <style jsx>
