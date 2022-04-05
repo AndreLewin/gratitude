06c8c10a450f93b176ac8a50143a75496ebf3bb8
AndreLewin <14772765+AndreLewin@users.noreply.github.com>
2022-04-05 12:09:46 +0200

    supabase and vercel do not provide a cron service

--- components/Privacy.tsx
+++ components/Privacy.tsx
@@ -9,6 +9,7 @@ export default function Privacy() {
       <div className="line">Messages with the "public" visibility level can be seen from everyone, even from people who don't have an account. They can be easily copied to other websites, so be careful about what you write in it.</div>
       <div className="line">The developer promises to never spy on content that is private or friend only.</div>
       <div className="line">The application stores no data that is not inherently needed for the goal of the application (social gratitude journaling).</div>
+      <div className="line">Everything about your account (messages, profile, friendship statuses...) might be deleted after one year of inactivity depending on storage capacity.</div>
       <div className="line">The applications uses <a className="link" href="https://supabase.com/" target="_blank" rel="noopener noreferrer">Supabase</a> for its backend, especially for user authentication and database storage. The developer believes Supabase is a trustable company that will not spy on and leak database information. Developers would not work with them if they do.</div>
       <div className="line">No data is shared with third-parties.</div>
       <div className="line">The application will never be for-profit.</div>
